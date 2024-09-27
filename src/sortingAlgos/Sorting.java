be8fdf41e8f93db8be867d29a00cf6aaa4508383
hmccormak <hmccormak@protonmail.com>
2024-09-27 12:22:48 -0400

    updated insertion sort

--- src/sortingAlgos/Sorting.java
+++ src/sortingAlgos/Sorting.java
@@ -1,28 +1,42 @@
 package sortingAlgos;
 
 import java.util.Random;
+import java.util.Arrays;
 
 public class Sorting {
-	int[] arr = new int[20];
+	int[] arr, orig;
 	
-	public Sorting() {
+	public Sorting(int size) {
+		this.arr = new int[size];
 		Random rand = new Random();
 		rand.setSeed(System.currentTimeMillis());
-		for (int i: arr) {
+		for (int i=0; i<arr.length; i++) {
 			arr[i] = rand.nextInt(100);
 		}
+		this.orig = Arrays.copyOf(arr, size);
 	}
 	
-	void insertionSort(int[] arr) {
-		int n = arr.length;
-		for (int k = 1; k < n; k++) {			// begin with second int
-			int cur = arr[k];					// time to insert cur = arr[k]
-			int j = k;							// find correct index j for cur
-			while(j > k && arr[j-1] > cur) {	// thus arr[j-1] must go after cur
-				arr[j] = arr[j-1];				// slide arr[j-1] rightward
-				j--;							// and consider previous j for cur
+	void insertionSort() {
+		int n = this.arr.length;
+		for (int k = 1; k < n; k++) {				// begin with second int
+			int cur = this.arr[k];					// time to insert cur = arr[k]
+			int j = k;								// find correct index j for cur
+			while(j > 0 && this.arr[j-1] > cur) {	// thus arr[j-1] must go after cur
+				this.arr[j] = this.arr[j-1];		// slide arr[j-1] rightward
+				j--;								// and consider previous j for cur
 			}
-			arr[j] = cur;						// this is the proper place for cur
+			this.arr[j] = cur;						// this is the proper place for cur
 		}
+		results(this.arr, this.orig);
+	}
+	
+	void results(int[] arr, int[] orig) {
+		System.out.println("Original array: " + Arrays.toString(orig));
+		System.out.println("After sorting: " + Arrays.toString(arr));
+	}
+	
+	public static void main(String[] args) throws Exception {
+		Sorting iSort = new Sorting(20);
+		iSort.insertionSort();
 	}
 }
